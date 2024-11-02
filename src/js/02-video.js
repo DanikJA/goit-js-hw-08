976ffc5c6c5e4384014c4b9f582558e395360f4a
Danik2023 <danik.kyrteu2@gmail.com>
2024-11-02 20:27:59 +0200

    homework2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,89 @@
+// import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
+
+
+// const TIME_KEY = 'videoplayer-current-time';
+// const iframe = document.querySelector('iframe');
+// const player = new Player (iframe);
+
+
+
+// const getCurrentTime = function (currentTime) {
+//     const timeSeconds = currentTime.seconds;
+//     localStorage.setItem(TIME_KEY, JSON.stringify(timeSeconds))
+// }
+
+// player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+// const savedTime = JSON.parse(localStorage.getItem(TIME_KEY));
+
+
+// if (savedTime) {
+// player.setCurrentTime(savedTime).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
+// }
+
+//  player.on('play', function() {
+//         console.log('played the video!');
+//     });
+
+//     player.getVideoTitle().then(function(title) {
+//         console.log('title:', title);
+//     });
+
+
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const TIME_KEY = "videoplayer-current-time"
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+const getCurrentTime = function (currentTime) {
+    const timeSeconds = currentTime.seconds;
+    localStorage.setItem(TIME_KEY, timeSeconds);
+}
+
+const savedTime = localStorage.getItem(TIME_KEY)
+
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+if (savedTime) {
+  player.setCurrentTime(parseFloat(savedTime)).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});  
+}
+
+ player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
 
