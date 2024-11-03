2c45d7bca9449fae89bb767790cf105d82db218b
Danik2023 <danik.kyrteu2@gmail.com>
2024-11-03 20:21:13 +0200

    worked with form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+const localKey = "feedback-form-state";
 
+const form = document.querySelector(".feedback-form");
+const textArea = document.querySelector("textarea");
+const emailInput = document.querySelector("input[type='email']");
+
+populateTextarea();
+
+form.addEventListener('submit', textSubmit);
+textArea.addEventListener('input', throttle(textInput, 500));
+emailInput.addEventListener('input', throttle(textInput, 500));
+
+function textSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem(localKey);
+}
+
+function textInput(event) {
+    const formData = {
+        email: emailInput.value,
+        message: textArea.value,
+    }
+    localStorage.setItem(localKey, JSON.stringify(formData));
+}
+
+
+function populateTextarea(){
+    const savedMessage = localStorage.getItem(localKey);
+    if (savedMessage) {
+       const { email, message } = JSON.parse(savedMessage);
+        textArea.value = message;
+        emailInput.value = email;
+    }
+}
